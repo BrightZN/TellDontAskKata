e09b5caa1663411f1c2c60b6afcef22eb82e8473
Ryan Esterhuysen <ryanesterh3@gmail.com>
2021-01-14 10:58:22 +0200

    Removed IProductCatalog.GetByNameAsync

--- TellDontAskKata/Repositories/IProductCatalog.cs
+++ TellDontAskKata/Repositories/IProductCatalog.cs
@@ -6,7 +6,6 @@ namespace TellDontAskKata.Repositories
 {
     public interface IProductCatalog
     {
-        Task<Product> GetByNameAsync(string name);
         Task<ProductList> GetForNamesAsync(IEnumerable<string> names);
     }
 }
