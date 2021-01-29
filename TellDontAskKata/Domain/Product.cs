a63356034cf0134f1b8c1ebfaae838d82079a58f
Ryan Esterhuysen <ryanesterh3@gmail.com>
2021-01-29 16:14:35 +0200

    Extracted and moved method CalculateUnitaryTax form Product.cs to Category.cs

--- TellDontAskKata/Domain/Product.cs
+++ TellDontAskKata/Domain/Product.cs
@@ -8,16 +8,13 @@ namespace TellDontAskKata.Domain
         public decimal Price { get; set; }
         public Category Category { get; set; }
 
-        private decimal CalculateUnitaryTax()
-        {
-            return decimal.Round(Price / 100.00M * Category.TaxPercentage, 2, MidpointRounding.AwayFromZero);
-        }
+        private decimal CalculateUnitaryTax() => Category.CalculateUnitaryTax(Price);
 
         public decimal CalculateUnitaryTax(int itemQuantity) => CalculateUnitaryTax() * itemQuantity;
 
         private decimal CalculateUnitaryTaxedAmount()
         {
-            return decimal.Round(Price + this.CalculateUnitaryTax(), 2, MidpointRounding.AwayFromZero);
+            return decimal.Round(Price + CalculateUnitaryTax(), 2, MidpointRounding.AwayFromZero);
         }
 
         public decimal CalculateTaxedAmount(int itemQuantity)
